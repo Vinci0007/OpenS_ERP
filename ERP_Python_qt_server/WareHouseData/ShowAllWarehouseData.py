386e4edee0da5dc9a69f171ee046db2344c9a2e1
Vincy Brove <39933794+Vinci0007@users.noreply.github.com>
2025-12-18 17:35:43 +0800

    Update ShowAllWarehouseData.py

--- ERP_Python_qt_server/WareHouseData/ShowAllWarehouseData.py
+++ ERP_Python_qt_server/WareHouseData/ShowAllWarehouseData.py
@@ -10,25 +10,29 @@ def showAllWarehouseData():
         allAllData_Warehouse = {}
         params = ['warehouse_product_infos']
         warehouseData = list(executor.map(mspool.query_table, params)) 
-        if warehouseData is not None:
-            for result in warehouseData[0]:   
-                if result:
-                    keys = ['id', 'product_id', 'product_type', 'product_name', 
-                            'supplier', 'destination', 'isoutbound', 'inbound_time', 'outbound_time', 'receive_time', 
-                            'price', 'price_unit', 
-                            'quality', 'quality_unit', 'operator', 'productCheckID'] 
-                    
-                    json_results = dict(zip(keys, result))
-                    user_check_id = json_results['operator']
-                    
-                    name_withcheckid = ['erp_account'], ['user_check_id'], [user_check_id]
-                    userinfo = list(executor.map(mspool.query_fields, *name_withcheckid)) 
-                    if userinfo is not None:
-                        json_results['operator'] = userinfo[0][0][2]
-                    allAllData_Warehouse.update({json_results['id']: json_results})
-                # json_results.update({'auth_token': Auth_token})
-            return allAllData_Warehouse
-        else:
+        if warehouseData is None:
             return {}
+        for result in warehouseData[0]:   
+            if result:
+                keys = ['id', 'product_id', 
+                        'product_type', 'product_name', 
+                        'supplier', 'destination', 
+                        'isoutbound', 'inbound_time', 
+                        'outbound_time', 'receive_time', 
+                        'price', 'price_unit', 
+                        'quality', 'quality_unit', 
+                        'operator', 'productCheckID'] 
+                
+                json_results = dict(zip(keys, result))
+                user_check_id = json_results['operator']
+                
+                name_withcheckid = ['erp_account'], ['user_check_id'], [user_check_id]
+                userinfo = list(executor.map(mspool.query_fields, *name_withcheckid)) 
+                if userinfo is not None:
+                    json_results['operator'] = userinfo[0][0][2]
+                allAllData_Warehouse.update({json_results['id']: json_results})
+            # json_results.update({'auth_token': Auth_token})
+        return allAllData_Warehouse
+            
  
  
